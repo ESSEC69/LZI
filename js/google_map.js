6b6870d00a43fc26b2f0d04ee9c555aa67e25e08
Cyrille Ducret <cyrille.cpi@gmail.com>
2017-07-17 21:04:57 +0200

    localisation

--- js/google_map.js
+++ js/google_map.js
@@ -35,11 +35,11 @@ function init() {
     for (var x = 0; x < addresses.length; x++) {
         $.getJSON('http://maps.googleapis.com/maps/api/geocode/json?address='+addresses[x]+'&sensor=false', null, function (data) {
             var p = data.results[0].geometry.location
-            var latlng = new google.maps.LatLng(p.lat, p.lng);
+            var latlng = new google.maps.LatLng(46.0479304, 5.5918828);
             new google.maps.Marker({
                 position: latlng,
                 map: map,
-                icon: '../images/loc.png'
+                icon: 'images/loc.png'
             });
 
         });
